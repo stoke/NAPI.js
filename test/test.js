0fd1f19dc767c5dff99e14c2cb2dbde35b1aa247
stoke <stoke95@gmail.com>
2013-01-06 15:56:01 +0000

    build files

--- test/test.js
+++
@@ -1,4 +0,0 @@
-NAPI.getProjectIdFromName("Turkish Hack Crew", NAPI.utils._asyncAssertEq(240));
-NAPI.getNameFromProjectId(240, NAPI.utils._asyncAssertEq("Turkish Hack Crew"));
-NAPI.getUserIdFromUserName("stoke", NAPI.utils._asyncAssertEq(1655));
-NAPI.getUsernameFromUserId(1655, NAPI.utils._asyncAssertEq("stoke"));
